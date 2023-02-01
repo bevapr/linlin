7ae766c33c8b687a1e789b085ce442f825b66fbd
bevapr <berlandvardy@gmail.com>
2023-02-01 11:22:55 +0700

    First commit

---
+++ application/views/footer.php
@@ -0,0 +1,6 @@
+				<section id="footer" class="page-section">
+					<footer id="footer-section" class="page-section pt-30 pb-30 center-xs">					
+						<div class="copyright fw300 mb-sm-10"><span>&copy; Created : Berland Vardy Pratama </span></div>
+						<div class="top-button"><h5>Top</h5><div class="line"></div></div>
+					</footer>
+				</section>
